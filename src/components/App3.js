26e71e119df9afd638110e73fa9a44ea6b3fd14f
Rebeca Gaia <reb.gaia@gmail.com>
2021-07-14 12:17:05 -0300

    using states - class 3

---
+++ src/components/App3.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class App3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: ''
+    };
+  }
+
+  changeName(e) {
+    this.setState({nome: e.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+        Nome: <input type="text" value={this.state.nome} onChange={this.changeName.bind(this)}></input>
+        <p>Olá {this.state.nome} </p>
+      </div>
+    );
+  }
+}
+
+export default App3;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import App2 from './components/App2';
+import App3 from './components/App3';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App2 title="Titulo">
-      Conteúdo
-    </App2>
-    <App2 title="Titulo2">
-      Conteúdo2
-    </App2>
+		<App3 />
+	
   </React.StrictMode>,
   document.getElementById('root')
-
-);
-
+  
+  );
+  
+  
